acc55c1f8b7f52004a616e47ac733b331726d97f
luckyshymal2906 <boirishyamal2906@gmail.com>
2024-03-04 23:18:22 +0530

    leetcode problem 1

---
+++ leetcode.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int arr[100],target,n;
+  cout<<"enter size of array: ";
+  cin>>n;
+  cout<<"enter array element: ";
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  cout<<"enter a target: ";
+  cin>>target;
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      if(arr[i]+arr[j]==target){
+        cout<<i<<","<<j<<endl;
+      }
+    }
+  }
+}
