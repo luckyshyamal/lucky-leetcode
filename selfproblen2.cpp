7562b606c36c1eb6ca3255a1137cf1aed6801f48
luckyshymal2906 <boirishyamal2906@gmail.com>
2024-03-05 23:44:28 +0530

    self problem 2

---
+++ selfproblen2.cpp
@@ -0,0 +1,33 @@
+
+//? find the different between the sum of elements at even indices to the sum of elements at the odd indices.
+
+#include<iostream>
+using namespace std;
+int main(){
+
+  //? initiating arrays
+  int arr[100],n;
+  cout<<"enter size of array: ";
+  cin>>n;
+  cout<<"enter array element: ";
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  
+
+  //? finding the sum of even and odd indices
+  int evenSum=0,oddSum=0,result;
+  for(int i=0;i<n;i++){
+   if(i%2==0){
+    evenSum+=arr[i];
+   }
+   else{
+    oddSum+=arr[i];
+   }
+  }
+  cout<<"even sum = "<<evenSum<<endl<<"odd sum = "<<oddSum<<endl;
+  //? subtracting oddSum to evenSum
+  result=evenSum-oddSum;
+  cout<<"difference between even and odd indices = "<<result;
+
+}
