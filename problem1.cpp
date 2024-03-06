ea695e16ce4e329250da776eebc292c51da686c1
luckyshymal2906 <boirishyamal2906@gmail.com>
2024-03-06 22:19:20 +0530

    add changes

--- problem1.cpp
+++ problem1.cpp
@@ -12,7 +12,7 @@ int main(){
   cout<<"enter a target: ";
   cin>>target;
   for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
+    for(int j=i+1;j<n;j++){
       if(arr[i]+arr[j]==target){
         cout<<i<<","<<j<<endl;
       }
