8ffe7b70ccc3775a14deb5981c96f5e183b96f5f
luckyshymal2906 <boirishyamal2906@gmail.com>
2024-03-06 23:20:40 +0530

    unique element problem

---
+++ uniqueElement.cpp
@@ -0,0 +1,33 @@
+
+//? find the unique elements from the array
+
+#include<iostream>
+using namespace std;
+int main(){
+  //? initiate the array
+  int arr[10],n;
+  cout<<"enter size of array: ";
+  cin>>n;
+  cout<<"enter array element: ";
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  //? find the same elements in the array and turn them to 0
+  for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+      if(arr[i]==arr[j]){
+        arr[i]=arr[j]=0;
+      }
+    }
+  }
+
+  //? check weather elements are 0 or nat not 
+  //? if not print the element 
+  cout<<"unique elements are: ";
+  for(int i=0;i<n;i++){
+    if(arr[i]!=0){
+      cout<<arr[i]<<" ";
+    }
+  }
+}
