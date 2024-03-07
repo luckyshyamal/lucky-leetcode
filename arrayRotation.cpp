8a180dc34cb4e69e39050a4867a63dad646d4349
luckyshymal2906 <boirishyamal2906@gmail.com>
2024-03-07 23:59:38 +0530

    rotaion of array

---
+++ arrayRotation.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int  main(){
+  //?initiating array elements
+  int arr[10],n;
+  cout<<"enter size of array: ";
+  cin>>n;
+  cout<<"enter array element: ";
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  //? ask for no. of rotation 
+  int k;
+  cout<<"enter number of rotation: ";
+  cin>>k;
+  int emp_arr[n],j=0;
+  //? if the rotation no. is greater than array element then find there reminder
+  k%=n;
+
+  
+  for(int i=n-k;i<n;i++){
+    emp_arr[j++]=arr[i];
+  }
+  for(int i=0;i<n-k;i++){
+    emp_arr[j++]=arr[i];
+  }
+  for(int i=0;i<n;i++){
+    cout<<emp_arr[i]<<" ";
+  }
+}
